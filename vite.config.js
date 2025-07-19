00c92d65adae7f10ac40d9dc174a1ef553493461
Ashok Patel <ashokpatel881095@gmail.com>
2025-07-20 00:51:06 +0530

    solving routing issue

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,9 +5,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
-  },
-  server: {
-    // optional for local dev fallback
-    historyApiFallback: true
   }
 });
